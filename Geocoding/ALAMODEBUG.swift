6435993b3b4dc8c49d99a153fdee794273b023d5
John <sparteclaudius749@gmail.com>
2018-12-11 21:35:21 +0100

    alamofire

--- Geocoding/ALAMODEBUG.swift
+++ Geocoding/ALAMODEBUG.swift
@@ -4,29 +4,122 @@ import UIKit
 
 
 
-final class JWTAccessTokenAdapter: RequestAdapter {
-    typealias JWT = String
-    var accessToken: JWT
+class StreetFitTokenHandler: RequestAdapter, RequestRetrier {
+    private typealias RefreshCompletion = (_ succeeded: Bool, _ jwt1: String, _ jwt2: String?) -> ()
     
-    let MemJwt1 = UserDefaults.standard.string(forKey: "jwt1")
-    let MemJwt2 = UserDefaults.standard.string(forKey: "jwt2")
+    private let sessionManager: SessionManager = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        
+        return SessionManager(configuration: configuration)
+        
+    } ()
     
+    private let lock = NSLock()
     
-    init(accessToken: JWT) {
-        self.accessToken = accessToken
+    private var jwt1: String
+    private var jwt2: String
+    private var baseURLString: String
+    
+    private var isRefreshing = false
+    private var requestsToRetry: [RequestRetryCompletion] = []
+    
+    // Initialization
+    
+    public init(jwt1: String,jwt2:String,baseURLString: String) {
+        self.jwt1 = jwt1
+        self.jwt2 = jwt2
+        self.baseURLString = baseURLString
     }
     
+    // Request Adapter
     
-    /// Adapter
     func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
-        var urlRequest = urlRequest
-        
-        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix("http://83.217.132.102:3000/") {
-            /// Set the Authorization header value using the access token.
-            urlRequest.setValue(MemJwt2, forHTTPHeaderField: "jwt2")
+        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix(baseURLString) {
+            var urlRequest = urlRequest
+            urlRequest.setValue(jwt1, forHTTPHeaderField: "jwt1")
+            return urlRequest
         }
         
         return urlRequest
+    }
+    
+    // RequestRetrier
+    
+    func should(_ manager: SessionManager, retry request: Request, with error: Error, completion: @escaping RequestRetryCompletion) {
+        if let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 {
+            requestsToRetry.append(completion)
+            
+            if !isRefreshing {
+                refreshToken(jwt1: <#T##String#>, completion: <#T##(String) -> Void#>)
+            }
+        }
+    }
+    
+    func refreshToken (jwt1: String, completion: @escaping RefreshCompletion) {
+        
+        let urlString = "http://83.217.132.102:3000/auth/refresh"
+        guard !isRefreshing else { return }
+        
+        isRefreshing = true
+        
+        let parameters: [String: Any] = [
+            "jwt1" : jwt1
+        ]
+        
+        let headers: HTTPHeaders = [
+            "jwt1" : jwt1
+        ]
+        
+        sessionManager.request(urlString, method: .post,encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            guard let strongSelf = self else { return }
+            
+            if
+                let json = response.result.value as? [String:Any],
+                let jwt1 = json["jwt1"] as? String,
+                let jwt2 = json["jwt2"] as? String
+            {
+                completion(true,jwt1,jwt2)
+            } else {
+                completion(false,nil,nil)
+            }
+            
+            strongSelf.isRefreshing = false
+            
+        }
+    }
+    
+    
+}
+
+
+class AugmentedLoginVC: UIViewController {
+
+    
+    @IBAction func ActionLaunch(_ sender: Any) {
+        Launch()
+    }
+    
+    @IBAction func ActionRefresh(_ sender: Any) {
+        let jwt1 = UserDefaults.standard.string(forKey: "jwt1")!
+        refreshToken(jwt1: jwt1) { response in
+            
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    func Launch() {
+        
+    let MemJwt2 = UserDefaults.standard.string(forKey: "jwt2")
+        
+    let sessionManager = SessionManager()
+    sessionManager.adapter = JWTAccessTokenAdapter(accessToken: MemJwt2!)
+    sessionManager.request("http://83.217.132.102:3000/auth/experlogin/innerjoin")
     }
     
     // RefreshRequest
@@ -44,6 +137,7 @@ final class JWTAccessTokenAdapter: RequestAdapter {
                 let decoder = JSONDecoder()
                 let model = try decoder.decode(MainResStruct.self, from: response.data!)
                 completion(model.data!.jwt2!)
+                print(model.data!.jwt2!)
                 
             } catch {}
             
@@ -51,49 +145,5 @@ final class JWTAccessTokenAdapter: RequestAdapter {
         
     }
     
-// End of class
-}
-
-// RequestRetrier
-extension JWTAccessTokenAdapter: RequestRetrier {
     
-    
-    func should(_ manager: SessionManager, retry request: Request, with error: Error, completion: @escaping RequestRetryCompletion) {
-        
-        guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 else {
-            completion(false, 0.0)
-            print("we are in success RequestRetier")
-            return
-        }
-        
-        refreshToken (jwt1: MemJwt1!) { [weak self] accessToken in
-            guard let strongSelf = self else { return }
-            
-            strongSelf.accessToken = accessToken
-            print("We are in refresh case RequestRetrier")
-            completion(true, 0.0)
-        }
-    }
-}
-
-
-class AugmentedLoginVC: UIViewController {
-    
-    @IBAction func ActionLaunch(_ sender: Any) {
-        Launch()
-    }
-    
-    
-    override func viewDidLoad() {
-        
-    }
-    
-    func Launch() {
-        
-    let MemJwt2 = UserDefaults.standard.string(forKey: "jwt2")
-        
-    let sessionManager = SessionManager()
-    sessionManager.adapter = JWTAccessTokenAdapter(accessToken: MemJwt2!)
-    sessionManager.request("http://83.217.132.102:3000/auth/experlogin/innerjoin")
-    }
 }
